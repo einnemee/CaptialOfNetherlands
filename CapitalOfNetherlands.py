e635c01e7da43cc0f8b3af5f839ece2da5fff28d
Elizabeth Innemee <55023647+einnemee@users.noreply.github.com>
2023-07-12 15:07:47 +0100

    Update CapitalOfNetherlands.py
    .upper() added to input

--- CapitalOfNetherlands.py
+++ CapitalOfNetherlands.py
@@ -1,14 +1,14 @@
 #What is the capital of The Netherlands?
 
-capitalGuess = input("What is the capital of The Netherlands? ")
+capitalGuess = input("What is the capital of The Netherlands? ").upper()
 numberOfGuesses = 1
 
-while capitalGuess != "Amsterdam":
+while capitalGuess != "AMSTERDAM":
     numberOfGuesses += +1
     if numberOfGuesses > 3:
         print("You guessed incorrectly three times. Game over.")
         break
     capitalGuess = input("Guess again ")
 
-if capitalGuess == "Amsterdam":
+if capitalGuess == "AMSTERDAM":
     print ("You guessed it. Amsterdam is the capital of The Netherlands. It took you ", numberOfGuesses, " guesses.")

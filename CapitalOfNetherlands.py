9cf2255430db5dc527dda0ad46177ce4f39510a1
Elizabeth Innemee <55023647+einnemee@users.noreply.github.com>
2023-07-12 07:44:32 +0100

    Update CapitalOfNetherlands.py
    Maximum 3 guesses added

--- CapitalOfNetherlands.py
+++ CapitalOfNetherlands.py
@@ -5,7 +5,10 @@ numberOfGuesses = 1
 
 while capitalGuess != "Amsterdam":
     numberOfGuesses += +1
+    if numberOfGuesses > 3:
+        print("You guessed incorrectly three times. Game over.")
+        break
     capitalGuess = input("Guess again ")
 
-print ("You guessed it. Amsterdam is the capital of The Netherlands")
-print ("It took you", numberOfGuesses, "guesses.")
+if capitalGuess == "Amsterdam":
+    print ("You guessed it. Amsterdam is the capital of The Netherlands. It took you ", numberOfGuesses, " guesses.")
